414c0988f87439a5696313be48160bf3bbe326c2
KarolBrz <61661483+Karol1302@users.noreply.github.com>
2022-05-12 20:29:33 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace OHVRacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gameoverbox = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.polonez = new System.Windows.Forms.PictureBox();
+            this.background2 = new System.Windows.Forms.PictureBox();
+            this.background1 = new System.Windows.Forms.PictureBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.score = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameoverbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polonez)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.gameoverbox);
+            this.panel1.Controls.Add(this.explosion);
+            this.panel1.Controls.Add(this.p2);
+            this.panel1.Controls.Add(this.p1);
+            this.panel1.Controls.Add(this.polonez);
+            this.panel1.Controls.Add(this.background2);
+            this.panel1.Controls.Add(this.background1);
+            this.panel1.Location = new System.Drawing.Point(12, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 607);
+            this.panel1.TabIndex = 0;
+            // 
+            // gameoverbox
+            // 
+            this.gameoverbox.Image = ((System.Drawing.Image)(resources.GetObject("gameoverbox.Image")));
+            this.gameoverbox.Location = new System.Drawing.Point(184, 246);
+            this.gameoverbox.Name = "gameoverbox";
+            this.gameoverbox.Size = new System.Drawing.Size(179, 136);
+            this.gameoverbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gameoverbox.TabIndex = 1;
+            this.gameoverbox.TabStop = false;
+            this.gameoverbox.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // explosion
+            // 
+            this.explosion.Image = ((System.Drawing.Image)(resources.GetObject("explosion.Image")));
+            this.explosion.Location = new System.Drawing.Point(386, 329);
+            this.explosion.Name = "explosion";
+            this.explosion.Size = new System.Drawing.Size(87, 121);
+            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.explosion.TabIndex = 1;
+            this.explosion.TabStop = false;
+            this.explosion.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // p2
+            // 
+            this.p2.Image = global::OHVRacing.Properties.Resources.police_lights_21;
+            this.p2.Location = new System.Drawing.Point(494, 26);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(63, 118);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2.TabIndex = 1;
+            this.p2.TabStop = false;
+            this.p2.Tag = "right";
+            this.p2.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // p1
+            // 
+            this.p1.Image = ((System.Drawing.Image)(resources.GetObject("p1.Image")));
+            this.p1.Location = new System.Drawing.Point(4, 44);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(61, 118);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1.TabIndex = 1;
+            this.p1.TabStop = false;
+            this.p1.Tag = "left";
+            this.p1.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // polonez
+            // 
+            this.polonez.BackColor = System.Drawing.Color.Black;
+            this.polonez.Image = ((System.Drawing.Image)(resources.GetObject("polonez.Image")));
+            this.polonez.Location = new System.Drawing.Point(235, 453);
+            this.polonez.Name = "polonez";
+            this.polonez.Size = new System.Drawing.Size(61, 137);
+            this.polonez.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.polonez.TabIndex = 1;
+            this.polonez.TabStop = false;
+            this.polonez.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // background2
+            // 
+            this.background2.Image = ((System.Drawing.Image)(resources.GetObject("background2.Image")));
+            this.background2.Location = new System.Drawing.Point(0, 0);
+            this.background2.Name = "background2";
+            this.background2.Size = new System.Drawing.Size(560, 607);
+            this.background2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background2.TabIndex = 0;
+            this.background2.TabStop = false;
+            // 
+            // background1
+            // 
+            this.background1.Image = ((System.Drawing.Image)(resources.GetObject("background1.Image")));
+            this.background1.Location = new System.Drawing.Point(0, -607);
+            this.background1.Name = "background1";
+            this.background1.Size = new System.Drawing.Size(560, 607);
+            this.background1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background1.TabIndex = 0;
+            this.background1.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(242, 651);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(100, 39);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // score
+            // 
+            this.score.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(12, 9);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(560, 23);
+            this.score.TabIndex = 2;
+            this.score.Text = "Score: 0";
+            this.score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.score.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 704);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(556, 63);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Press left and right to move the Polonez\r\nEscape from the Police and watch out, b" +
+    "ecause the Polonez is beast!\r\n\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.TimeEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 811);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OHV RACING";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.downkey);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.upkey);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gameoverbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polonez)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox polonez;
+        private System.Windows.Forms.PictureBox background2;
+        private System.Windows.Forms.PictureBox background1;
+        private System.Windows.Forms.PictureBox explosion;
+        private System.Windows.Forms.PictureBox gameoverbox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OHVRacing
+{
+    public partial class Form1 : Form
+    {
+        int playerspeed = 25;
+        int policespeed = 12;
+        int points;
+        int backgroundspeed = 35;
+        int gametime;
+        int carimage;
+
+        bool goleft, goright;
+
+        Random R = new Random();
+        Random carposition = new Random();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetgame();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void downkey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+            if(e.KeyCode==Keys.Left)
+            {
+                goleft = true;
+            }
+
+        }
+
+        private void TimeEvent(object sender, EventArgs e)
+        {
+            gameoverbox.Visible = false;
+            score.Text = "Score: " + points;
+            points++;
+
+
+            if(goleft==true && polonez.Left >10)
+            {
+                polonez.Left -= playerspeed;
+            }
+            if (goright == true && polonez.Left < 490)
+            {
+                polonez.Left += playerspeed;
+            }
+            //to zadziała tak że gracz nie opuści panelu a nawet nie opuści lokalizacji. 4-494
+
+            background1.Top += backgroundspeed;
+            background2.Top += backgroundspeed; //poprzez int backgroundspeed będą się oba te tła przesuwać w dół
+
+            if (background2.Top > 607)
+            {
+                background2.Top = -607;
+            }
+            if (background1.Top > 607)
+            {
+                background1.Top = -607;
+            }
+
+            p1.Top += policespeed;
+            p2.Top += policespeed;
+
+            if(p1.Top > 610)
+            {
+                changecar(p1);      
+            }
+            if (p1.Top > 610)
+            {
+                changecar(p2);
+            }
+
+            if (polonez.Bounds.IntersectsWith(p1.Bounds) || polonez.Bounds.IntersectsWith(p2.Bounds)) //kolizje
+            {
+                gameover();
+            }
+            if(points < 500)
+            {
+                policespeed = 15;
+                backgroundspeed = 30;
+            }
+            if(points > 500 && points <2000)
+            {
+                policespeed = 44;
+                backgroundspeed = 45;
+            }
+            if (points > 2000)
+            {
+                policespeed = 60;
+                backgroundspeed = 60;
+            }
+        } 
+
+        private void upkey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+        }
+        private void changecar(PictureBox change)       //picturebox jest przesłany do tej funkcji
+        {
+           carimage = R.Next(1,2);     //losuje licze od 1 do 2 aby zmieniac radiowozy
+            if(carimage == 1)
+                change.Image = Properties.Resources.police_lights;
+            if(carimage == 2)
+                change.Image = Properties.Resources.police_lights_21;
+
+            change.Top = carposition.Next(100,400) * -1;
+
+            if((string)change.Tag == "left")
+            {
+                change.Left = carposition.Next(3, 194);
+            }
+            if ((string)change.Tag == "right")
+            {
+                change.Left = carposition.Next(300, 494);
+            }
+
+
+            
+        }
+        private void gameover()
+        {
+            playsound();
+            timer1.Stop();
+            explosion.Visible = true;
+            polonez.Controls.Add(explosion); //parent do polonez żeby ustawić lokalizacje na nim
+            explosion.Location = new Point(-8,5);
+            explosion.BackColor = Color.Transparent;
+            gameoverbox.Visible = true;
+            
+            StartButton.Enabled = true;
+        }
+        private void resetgame()
+        {
+            StartButton.Enabled = false;
+            explosion.Visible = false;
+            goleft = false;
+            goright = false;
+            backgroundspeed = 10;
+            policespeed = 13;
+
+            p1.Top = carposition.Next(200, 500) * -1;   //losuje połozenie radiowozu, mnozenie razy -1 żeby wyglądało że porusza się w dół
+            p1.Left = carposition.Next(3, 194);         //ograniczenie gdzie może się pojawic radiowóz
+
+            p2.Top = carposition.Next(200, 500) * -1;
+            p2.Left = carposition.Next(300, 494);
+
+            timer1.Start();
+        }
+        private void playmusic()
+        {
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            resetgame();
+        }
+
+        private void playsound()
+        {
+            System.Media.SoundPlayer soundcrash = new System.Media.SoundPlayer(Properties.Resources.Car_Crash_Sound_Effects);//nowa instancja ten przestrzeni nazw sysytem media 
+            soundcrash.Play();
+
+        }
+
+    }
+}
